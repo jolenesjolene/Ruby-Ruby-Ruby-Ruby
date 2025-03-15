04117cc86f24b1c54a1117acc240905e3896a3af
jolenesjolene <imfullof137@gmail.com>
2025-03-15 13:20:02 +0000

    initial commit

---
+++ src/main/java/net/jolene/rubyrubyrubyruby/RubyRubyRubyRubyClient.java
@@ -0,0 +1,10 @@
+package net.jolene.rubyrubyrubyruby;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RubyRubyRubyRubyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/jolene/rubyrubyrubyruby/RubyRubyRubyRubyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.jolene.rubyrubyrubyruby;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class RubyRubyRubyRubyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
